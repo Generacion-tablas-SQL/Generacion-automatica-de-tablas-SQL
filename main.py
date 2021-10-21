630455d94c1d6efddd7d59fa82f17d8c25423593
AlvaroPlazaSanz <55554546+AlvaroPlazaSanz@users.noreply.github.com>
2021-10-21 13:09:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 
-
 import random
 from random import randint
 
 # Para estos generadores de los diferentes tipos de datos tenemos en cuenta las restricciones de tipo:
-# UNIQUE, PRIMARY KEY, REFERENCES , *NOT NULL, *CHECK Y LENGTH
+# *UNIQUE, PRIMARY KEY, REFERENCES , *NOT NULL, *CHECK Y LENGTH
 
 
-# Generador de numeros enteros
-# INT = INTEGER
+# Generador de números enteros
+# id = INT or INTEGER
+# opt1 = null or nullable
+# opt2 = unique (no implementado )
 
 def generate_int(id, opt1, opt2):
 
@@ -18,15 +19,11 @@ def generate_int(id, opt1, opt2):
             print(n)
 
 
-generate_int("INT", "", "")
+#generate_int("INT", "", "")
+generate_int("INT", "NULL", "")
 #generate_int("INTEGER")
 #generate_int("inTeGer")
 #generate_int("integer")
-
-
-
-
-
 
 
 
