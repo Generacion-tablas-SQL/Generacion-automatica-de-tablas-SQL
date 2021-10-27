767ceb69e628fa76637c0d41af67c52985137325
LlucBonet <mbonet03@ucm.es>
2021-10-27 21:28:24 +0200

    genera enteros con o sin parámetros

--- main2.py
+++ main2.py
@@ -38,16 +38,18 @@ def generate_int2(column, constraint):
     if key[0] not in constantes.ENTEROS:
         return "Este tipo de datos no es un entero"
     else:
-        if key[0] != "number" and str(restriction[0]) != "{}":
+        if key[0] != "number" and str(restriction[0]) != "{}":  # Ej: {integer : 9}
             return "Este tipo de datos no soporta parámetros"
-        if key[0] == "number" and str(restriction[0]) == "{}":
-            return "Este tipo de datos es un número real"
-        if key[0] == "number" and len(restriction) == 2 and restriction[1] != 0:
-            return "Este tipo de datos es un número real"
-        if key[0] == "number":
+        if key[0] == "number" and str(restriction[0]) == "{}":  # {number: {}}
+            return "Este tipo de datos es un número real, no un entero"
+        if key[0] == "number" and isinstance(restriction[0], int):  # {number: 5}
             return random.randint(0, number_of_digits(restriction[0]))
+        if key[0] == "number" and len(restriction[0]) == 2 and restriction[0][1] != 0:  # Ej: {number : [4, 2]}
+            return "Este tipo de datos es un número real, no un entero"
+        if key[0] == "number" and len(restriction[0]) == 2:  # Ej: {number: 4, 0}
+            return random.randint(0, number_of_digits(restriction[0][0]))
         return random.randint(0, number_of_digits(38))  # int, integer y smallint tienen una precision de 38
 
 
-print(generate_int2({'name': 'id', 'type': {'integer': {}}, 'option': {'check': {'gt': ['Id', 50]}}},
+print(generate_int2({'name': 'id', 'type': {'number': [4, 0]}, 'option': {'check': {'gt': ['Id', 50]}}},
                     "{'name': 'NombreLargo', 'check': {'gt': [{'length': 'Nombre'}, 5]}}}"))
