1e550f3cf9afdd92c79df6288995b819c0008753
LlucBonet <mbonet03@ucm.es>
2021-11-17 00:33:35 +0100

    cambio probabilidad null

--- src/constantes.py
+++ src/constantes.py
@@ -1,4 +1,4 @@
-NULL_PROBABILITY = 0.2
+NULL_PROBABILITY = 0.1
 ENTEROS = ["int", "integer", "smallint"]  # NUMBER(p) y NUMBER(p, 0) son enteros
 COMPARADORES = ["eq", "neq", "gt", "gte", "lt", "lte"]
 REALES = ["number", "float", "numeric", "decimal", "dec", "real"]  # double precision no soportado por mo-sql-parsing
