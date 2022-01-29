a0bef9da086c67a21c1ade8b7fccd550e11c074f
LlucBonet <mbonet03@ucm.es>
2022-01-29 19:27:23 +0100

    correcciones varias

--- src/clasificador.py
+++ src/clasificador.py
@@ -1,7 +1,6 @@
 import constantes
 import generador_datos as gd
 
-
 # <----GENERAL---->
 def restricciones_sql(parameters, column):
     """Comprueba las restricciones SQL.
@@ -29,7 +28,7 @@ def restricciones_sql(parameters, column):
         restricciones_list[-1].update(comprobar_restricciones_check(parameters, check))
     else:  # parameters[0] == "Fecha"
         # POR EL MOMENTO LAS FECHAS NO POSEEN RESTRICCIONES CHECK
-        restricciones_list[-1].update({"sec_precision": parameters[1], "es_date": parameters[2]})
+        restricciones_list[-1].update({"sec_precision": parameters[2], "es_date": parameters[3]})
 
     return restricciones_list
 
@@ -48,39 +47,30 @@ def get_index(comparison, _not):
 def comprobar_restricciones_check(parameters, check):
     """Comprueba las restricciones CHECK
 
-        :param parameters: tipo de dato Number: ["Number", precision, escala]
-                           tipo de dato String: ["String", varying, max_size]
+        :param parameters: tipo de dato Number: ["Number", nombre_col, precision, escala]
+                           tipo de dato String: ["String", nombre_col, varying, max_size]
         :param check: campo check de la sentencia parseada
         :return: diccionario con el número máximo, mínimo equal y not equal si se específica
         """
 
-    _max = gd.max_number(parameters[1], parameters[2]) if parameters[0] == "Number" else parameters[2]
-    _min = -_max if parameters[0] == "Number" else (1 if parameters[1] is True else parameters[2])
+    _max = gd.max_number(parameters[2], parameters[3]) if parameters[0] == "Number" else parameters[3]
+    _min = -_max if parameters[0] == "Number" else (1 if parameters[2] is True else parameters[3])
     _eq = None
     _neq = None
     _not = None
     _like = None
-    _scale = None if parameters[0] == "String" else parameters[2]
+    _scale = 0 if parameters[0] == "String" else parameters[3]
+    _other = False
 
     if check is None:
-        return {"min": _min, "max": _max, "eq": _eq, "neq": _neq, "like": _like, "scale": _scale}
-
-    # operator = list(check.keys())[0].lower()  # primer operador que aparece en el check
-
-    # Crea una lista con la/las comparación/comparaciones
-    # if operator == "and" or operator == "or":
-    #    comparisons = check.get(operator)
-    # else:
-    #    comparisons = [check]
+        return {"min": _min, "max": _max, "eq": _eq, "neq": _neq, "like": _like, "scale": _scale, "other": _other}
 
     comparisons = check.get("args")
 
     for comparison in comparisons:
 
-        # comparison_op = list(comparison.keys())[0]
         comparison_op = comparison.get("op")
         if comparison_op == "not":
-            # _not = list(comparison.get("not").keys())[0]  # _not contiene: "lte", "gt"...
             _not = comparison.get("args")[0].get("op")  # _not contiene: "lte", "gt"...
         if comparison_op == "eq":
             _eq = comparison.get("args")[get_index(comparison, None)]
@@ -92,28 +82,30 @@ def comprobar_restricciones_check(parameters, check):
                 _max = min(_max, comparison.get("args")[0].get("args")[get_index(comparison, _not)])
             else:
                 # máximo entre _min y el contenido del comparador
-                # +1 para simplificar, si es de tipo numero decimal, debería sumarse el decimal más pequeño
-                _min = max(_min, comparison.get("args")[get_index(comparison, None)] + 1)
+                # Si es de tipo número decimal, se suma uno al decimal menos significativo
+                _min = max(_min, comparison.get("args")[get_index(comparison, None)] + 1 / 10 ** _scale)
         elif comparison_op == "gte" or _not == "gte":
             if _not == "gte":
-                _max = min(_max, comparison.get("args")[0].get("args")[get_index(comparison, _not)] - 1)
+                _max = min(_max, comparison.get("args")[0].get("args")[get_index(comparison, _not)] - 1 / 10 ** _scale)
             else:
                 _min = max(_min, comparison.get("args")[get_index(comparison, None)])
         elif comparison_op == "lt" or _not == "lt":
             if _not == "lt":
                 _min = max(_min, comparison.get("args")[0].get("args")[get_index(comparison, _not)])
             else:
-                _max = min(_max, comparison.get("args")[get_index(comparison, None)] - 1)
+                _max = min(_max, comparison.get("args")[get_index(comparison, None)] - 1 / 10 ** _scale)
         elif comparison_op == "lte" or _not == "lte":
             if _not == "lte":
-                _min = max(_min, comparison.get("args")[0].get("args")[get_index(comparison, _not)] + 1)
+                _min = max(_min, comparison.get("args")[0].get("args")[get_index(comparison, _not)] + 1 / 10 ** _scale)
             else:
                 _max = min(_max, comparison.get("args")[get_index(comparison, None)])
         elif comparison_op == "like":
             _like = comparison.get("args")[1].get("literal")
+        else:
+            _other = True
         _not = None
 
-    return {"min": _min, "max": _max, "eq": _eq, "neq": _neq, "like": _like, "scale": _scale}
+    return {"min": _min, "max": _max, "eq": _eq, "neq": _neq, "like": _like, "scale": _scale, "other": _other}
 
 
 # <----INICIO---->
@@ -130,31 +122,36 @@ def clasificar_tipo(columnas):
 
     for column in columnas:
         col_name = column.get("name").lower()
-        # data_type = list(column.get("type").keys())[0].lower()  # number
         data_type = column.get("type").get("op").lower()  # number
-        # parameters = list(column.get("type").values())  # [5, 0], [4]
         parameters = column.get("type").get("args", None)  # [5, 0], [4]
 
         if data_type in constantes.ENTEROS or data_type in constantes.REALES:
             data_type_param = list()
+            data_type_param.append("Number")
+            data_type_param.append(col_name)
             if data_type in constantes.ENTEROS:
-                data_type_param.append(38) if parameters is None else parameters
+                data_type_param.append(38) if parameters is None else data_type_param.append(parameters)
                 data_type_param.append(0)  # Escala = 0
             else:
-                data_type_param = [10, 4] if data_type == "float" or data_type == "real" else (
-                    [38, 127] if parameters is None else parameters)
+                data_type_param.extend([10, 4]) if data_type == "float" or data_type == "real" else (
+                    data_type_param.extend([38, 127]) if parameters is None else data_type_param.extend(parameters))
 
-                if len(data_type_param) == 1:
+                if len(data_type_param) == 3:
                     data_type_param.append(0)
 
-            data_type_param.insert(0, "Number")
             restricciones = restricciones_sql(data_type_param, column)
             restricciones[-1].update({"tipo": data_type})
             col_restrictions.update({col_name: restricciones})
 
             numbers = []
-            for i in range(10):
-                numbers.append(gd.generate_number(restricciones[-1]))
+            if restricciones[-1].get("other"):
+                for i in range(10):
+                    numbers.append(gd.generate_random(data_type_param[0], data_type_param[1], restricciones[-1],
+                                                      column.get("check")))
+            else:
+                for i in range(10):
+                    numbers.append(gd.generate_number(restricciones[-1]))
+
             col_data.update({col_name: numbers})
 
         elif data_type in constantes.STRINGS:
@@ -162,14 +159,19 @@ def clasificar_tipo(columnas):
             max_size = 1 if parameters is None else parameters[0]
             varying = False if data_type == "char" or data_type == "nchar" else True
 
-            data_type_param = ["String", varying, max_size]
+            data_type_param = ["String", col_name, varying, max_size]
             restricciones = restricciones_sql(data_type_param, column)
             restricciones[-1].update({"tipo": data_type})
             col_restrictions.update({col_name: restricciones})
 
             data = []
-            for i in range(10):
-                data.append(gd.generate_string(restricciones[-1]))
+            if restricciones[-1].get("other"):
+                for i in range(10):
+                    data.append(gd.generate_random(data_type_param[0], data_type_param[1], restricciones[-1],
+                                                   column.get("check")))
+            else:
+                for i in range(10):
+                    data.append(gd.generate_string(restricciones[-1]))
             col_data.update({col_name: data})
 
         elif data_type in constantes.FECHA:
@@ -177,7 +179,7 @@ def clasificar_tipo(columnas):
             es_date = 1 if data_type == "date" else 0
             sec_precision = 0 if parameters is None else parameters[0]
 
-            data_type_param = ["Fecha", sec_precision, es_date]
+            data_type_param = ["Fecha", col_name, sec_precision, es_date]
 
             # restricciones = ['unique', {'sec_precision': 2, 'es_date': 0}]
             restricciones = restricciones_sql(data_type_param, column)
@@ -193,5 +195,4 @@ def clasificar_tipo(columnas):
             col_data.update({col_name: fechas})
         else:
             raise ValueError
-            # print("Ha habido un error en la clasificación de tipo de datos.")
     return col_data, col_restrictions
